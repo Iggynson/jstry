731aedfe48e701464cdeb501e250c72602b8adbd
Ilya <iggynson@gmail.com>
2025-01-09 16:22:06 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let userName = 'Ilya'; 
+
+console.log(typeof(userName)); 
+
+
+const isChecked = true,
+			isClose   = false; 
+
+
+console.log(!isClose); 
+
+
+console.log(2 * 3 ** 2); 
+
+console.log(2 + 2 * 2 != '6'); 
